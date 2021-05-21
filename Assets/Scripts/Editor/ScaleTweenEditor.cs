6d9f1bdfbca324838c836c4f07c5e10447df1e81
Jeff Smith <toxicglados@gmail.com>
2021-05-20 19:54:58 -0500

    Move bonus words into popup menu

---
+++ Assets/Scripts/Editor/ScaleTweenEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ScaleTween))]
+public class ScaleTweenEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        ScaleTween scaleTween = (ScaleTween)target;
+        if(GUILayout.Button("Set target scale")){
+            scaleTween.SetTargetScale();
+        }
+    }
+}
---
+++ Assets/Scripts/Tweens/ScaleTween.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ScaleTween : MonoBehaviour
+{
+    public Vector3 targetScale;
+    [Range(0, 10)]
+    public float duration;
+    public LeanTweenType easeType;
+    public UnityEvent onStartCallback;
+    public UnityEvent onCompleteCallback;
+    public GameObject target;
+    public bool activate;
+    public void Tween(){
+        if(onStartCallback != null){
+            onStartCallback.Invoke();
+        }
+        LeanTween.scale(target, targetScale, duration).setEase(easeType).setOnComplete(OnComplete);
+    }
+
+    public void OnComplete(){
+        if(onCompleteCallback != null){
+            onCompleteCallback.Invoke();
+        }
+    }
+
+    public void SetTargetScale(){
+        targetScale = transform.localScale;
+    }
+}
