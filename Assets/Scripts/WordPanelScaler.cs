951560f44fa960059232196e437b74b77b2cd981
Jeff Smith <toxicglados@gmail.com>
2021-05-23 20:35:08 -0500

    Move WordPanelScaler to parent so that it can be put it can be driven by the SetLayout methods

--- Assets/Scripts/WordPanelScaler.cs
+++ Assets/Scripts/WordPanelScaler.cs
@@ -7,14 +7,15 @@ using UnityEngine.UI.Extensions;
 public class WordPanelScaler : MonoBehaviour, ILayoutSelfController
 {
     public float maxScale = 1.5f;
+    public RectTransform childRectTransform;
     RectTransform rectTransform;
-    RectTransform parentRectTransform;
-    LayoutGroup layoutGroup;
+    public LayoutGroup layoutGroup;
     float scaleFactor = 0.99f;
-    bool scalingUp = false;
+    public bool scale = false;
     // Keep track of the size for which the current scale and rect are valid
     // Prevents us from trying to ScaleUp every frame when we're good
-    Vector2 validForSize;
+    public Vector2 validForSize;
+    public float validForWidth;
     
     void Start(){
         SetupReferences();
@@ -22,67 +23,71 @@ public class WordPanelScaler : MonoBehaviour, ILayoutSelfController
 
     void SetupReferences(){
         rectTransform = GetComponent<RectTransform>();
-        layoutGroup = GetComponent<LayoutGroup>();
-        parentRectTransform = transform.parent.GetComponent<RectTransform>();
     }
 
     void ScaleDown(){
-        while(layoutGroup.minWidth > rectTransform.rect.size.x){
-            rectTransform.localScale *= scaleFactor;
+        while(layoutGroup.minWidth > childRectTransform.rect.size.x){
+            childRectTransform.localScale *= scaleFactor;
             FitToParent();
             // We have to rebuild the layout because the FlowLayout might move stuff around as we scale
             // and we need to take that into account on the next iteration
-            LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
+            LayoutRebuilder.ForceRebuildLayoutImmediate(childRectTransform);
         }
     }
 
     void ScaleUp(){
-        while(layoutGroup.minWidth < rectTransform.rect.size.x && rectTransform.localScale.x < maxScale){
-            rectTransform.localScale *=  1 + (1 - scaleFactor);
+
+        Vector2 startScale = childRectTransform.localScale;
+        while(layoutGroup.minWidth < childRectTransform.rect.size.x && childRectTransform.localScale.x < maxScale){
+            startScale = childRectTransform.localScale;
+            childRectTransform.localScale *=  1 + (1 - scaleFactor);
             FitToParent();
+            
             // We have to rebuild the layout because the FlowLayout might move stuff around as we scale
             // and we need to take that into account on the next iteration
-            LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
+            LayoutRebuilder.ForceRebuildLayoutImmediate(childRectTransform);
+        }
+        // The last iteration will set us over so we need to downsize to fit again
+        if(layoutGroup.minWidth > childRectTransform.rect.size.x){
+            childRectTransform.localScale = startScale;
+            FitToParent();
+            validForSize = childRectTransform.rect.size;
+            validForWidth = layoutGroup.minWidth;
         }
     }
 
     void FitToParent(){
-        Vector2 parentRectSize = parentRectTransform.rect.size;
-        Vector2 scaleRatio  = parentRectSize / (parentRectSize * rectTransform.localScale);
+        Vector2 parentRectSize = rectTransform.rect.size;
+        Vector2 scaleRatio  = parentRectSize / (parentRectSize * childRectTransform.localScale);
         Vector2 newRectSize = parentRectSize * scaleRatio;
         Vector2 delta = newRectSize - parentRectSize;
 
-        rectTransform.sizeDelta = delta;
+        childRectTransform.sizeDelta = delta;
     }
 
     void Update(){
-        FindBestScale();
+        //FindBestScale();
     }
 
     void FindBestScale(){
-        if(rectTransform.rect.size != validForSize){
+        
+        // If either childRectTransform.rect.size or layoutGroup.minWidth change than we need to re-layout
+        bool alreadyValidated = childRectTransform.rect.size == validForSize && layoutGroup.minWidth == validForWidth;
+        
+        if(!alreadyValidated){
+            LayoutRebuilder.ForceRebuildLayoutImmediate(childRectTransform);
             FitToParent();
-            if(layoutGroup.minWidth > rectTransform.rect.size.x && !scalingUp){
+            if(layoutGroup.minWidth > childRectTransform.rect.size.x){
                 ScaleDown();
             }
-            else if(layoutGroup.minWidth < rectTransform.rect.size.x && rectTransform.localScale.x < maxScale){
-                print("Trying to scale up");
-                Vector2 startScale = rectTransform.localScale;
-                scalingUp = true;
+            else if(layoutGroup.minWidth < childRectTransform.rect.size.x && childRectTransform.localScale.x < maxScale){                    
                 ScaleUp();
-                // If scaling up put us over the limit than revert to startScale
-                if(layoutGroup.minWidth > rectTransform.rect.size.x){
-                    rectTransform.localScale = startScale;
-                    FitToParent();
-                    validForSize = rectTransform.rect.size;
-                }
             }
         }
     }
 
     public void SetLayoutHorizontal(){
-        FitToParent();
-        //FindBestScale();
+        FindBestScale();
     }
 
     public void SetLayoutVertical(){
