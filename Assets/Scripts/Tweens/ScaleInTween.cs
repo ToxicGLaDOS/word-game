836546bcf0841274bb3286dacdbf258abe741574
Jeff Smith <toxicglados@gmail.com>
2021-05-30 01:08:24 -0500

    Scale input panel and word panel out on level end

--- Assets/Scripts/Tweens/ScaleInTween.cs
+++ Assets/Scripts/Tweens/ScaleInTween.cs
@@ -6,12 +6,12 @@ using UnityEngine.Events;
 public class ScaleInTween : MonoBehaviour
 {
     public Vector3 startScale;
+    public Vector3 endScale;
     [Range(0, 10)]
     public float duration;
     public LeanTweenType easeType;
     public UnityEvent onCompleteCallback;
     public void Tween(){
-        Vector3 endScale = transform.localScale;
         transform.localScale = startScale;
         LeanTween.scale(gameObject, endScale, duration).setEase(easeType).setOnComplete(OnComplete);
     }
